557017d3e031994f0dab90365f970d315499cd80
Bertrand Martel <bmartel.fr@gmail.com>
2016-07-07 15:22:12 +0200

    setup recyclerview for bitmap

---
+++ android/app/src/main/java/com/github/akinaru/bleremote/adapter/BitmapAdapter.java
@@ -0,0 +1,86 @@
+package com.github.akinaru.bleremote.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.github.akinaru.bleremote.R;
+import com.github.akinaru.bleremote.inter.IViewHolderClickListener;
+
+import java.util.List;
+
+public class BitmapAdapter extends RecyclerView.Adapter<BitmapAdapter.ViewHolder> {
+
+    private List<Integer> mBitmapList;
+
+    private IViewHolderClickListener mListener;
+
+    private Context mContext;
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public BitmapAdapter(Context context, List<Integer> bitmapList, IViewHolderClickListener listener) {
+        mContext = context;
+        mBitmapList = bitmapList;
+        this.mListener = listener;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public BitmapAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.bitmap_card, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        ViewHolder vh = new ViewHolder(v, mListener);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        int item = mBitmapList.get(position);
+        holder.bitmap.setImageResource(item);
+        if (item == R.drawable.ic_control_point) {
+            holder.bitmap.setPadding(0, 0, 0, 0);
+            holder.bitmap.setScaleType(ImageView.ScaleType.CENTER);
+        }
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mBitmapList.size();
+    }
+
+    /**
+     * ViewHolder for HCI packet
+     */
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public ImageView bitmap;
+
+        public IViewHolderClickListener mListener;
+
+        /**
+         * ViewHolder
+         *
+         * @param v
+         * @param listener
+         */
+        public ViewHolder(View v, IViewHolderClickListener listener) {
+            super(v);
+            mListener = listener;
+            bitmap = (ImageView) v.findViewById(R.id.logo_image);
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            mListener.onClick(v);
+        }
+    }
+}
